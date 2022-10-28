4c9a374864845e2b7b81d3c06bb74d423607c0bd
Masha Drapkin <53053775+mashadra@users.noreply.github.com>
2022-10-28 15:24:44 -0400

    Update with a reset button and ability to change size/point at top of document

--- pyqt53Dgraph.py
+++ pyqt53Dgraph.py
@@ -1,36 +1,63 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout,QWidget
 from pyqtgraph import opengl, glColor
 import numpy
 
+SIZE = 200
+
+# coordinates to place the point at
+x = 50
+y = 50
+z = 50
+
+# overall app
 app = QApplication([])
 
-# defining a top-level widget to hold everything
+# defining a top-level widget to hold everything for the graph
 w = opengl.GLViewWidget()
+
+# a button to reset rotation and zoom
+def reset_view(self):
+    global w
+    w.setCameraPosition(distance=SIZE*2, elevation=20, azimuth=45)
+    w.show()
+
+reset_button = QPushButton()
+reset_button.setText("Reset")
+reset_button.clicked.connect(reset_view)
 
 # making plot
 arr = []
-for i in range(0,200):
+for i in range(0,SIZE):
     arr += [(0, 0, i), (0, i, 0), (i, 0, 0)]
 axis_pts = numpy.array(arr)
 axis_clrs = glColor("#ffffff")
 axis_sze = 0.1
 
-pts = numpy.array([(50, 50, 50)])
+pts = numpy.array([(x, y, z)])
 clrs = glColor("#0004ff")
 sze = 10
 
+# making one point
 graph = opengl.GLScatterPlotItem()
 graph.setData(pos=pts, color=clrs, size=sze)
 
+# making points for axis
 axis = opengl.GLScatterPlotItem()
 axis.setData(pos=axis_pts, color=axis_clrs, size=axis_sze)
 
 # adding components to layout
 w.addItem(axis)
 w.addItem(graph)
-w.setCameraPosition(distance=400)
-w.orbit(0, 0)
+reset_view(w)
 
-w.show()
+# setting up the view
+layout = QVBoxLayout()
+layout.addWidget(reset_button)
+
+# making a window, adding the layout to it, and displaying it
+window = QWidget()
+window.setWindowTitle("Plot")
+window.setLayout(layout)
+window.show()
 
 app.exec_()
